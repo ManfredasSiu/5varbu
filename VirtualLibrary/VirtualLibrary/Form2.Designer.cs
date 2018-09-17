7a7b2b5b71b7c0e952417f14a536f710a83b1fcb
Austėja <austejaisgdr@users.noreply.github.com>
2018-09-17 22:53:03 +0300

    Super
    Gerai viskas, super, liux

--- VirtualLibrary/VirtualLibrary/Form2.Designer.cs
+++ VirtualLibrary/VirtualLibrary/Form2.Designer.cs
@@ -29,11 +29,90 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.RegisterButt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 68);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(661, 370);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // RegisterButt
+            // 
+            this.RegisterButt.Location = new System.Drawing.Point(12, 24);
+            this.RegisterButt.Name = "RegisterButt";
+            this.RegisterButt.Size = new System.Drawing.Size(75, 23);
+            this.RegisterButt.TabIndex = 3;
+            this.RegisterButt.Text = "Register";
+            this.RegisterButt.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Prisijungimo vardas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Slaptazodis";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RegisterButt);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button RegisterButt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- VirtualLibrary/VirtualLibrary/Form2.cs
+++ VirtualLibrary/VirtualLibrary/Form2.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +15,91 @@ namespace VirtualLibrary
 {
     public partial class Form2 : Form    //Register Langas, padarysiu irgi su face recognition
     {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetect;
+        Image<Bgr, Byte> frame;
+        Capture cam;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainImg = null;
+        Image<Gray, byte> GrayFace = null;
+        
+        
+       // List<string> users = new List<string>();
+        int count = 0, numLablels, t;
+        string name, names = null;
+
         private LogicController LogicC;
+
         public Form2(LogicController LogicC)
         {
             InitializeComponent();
             this.LogicC = LogicC;
+            faceDetect = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string labelsInf = File.ReadAllText(Application.StartupPath + "/faces/faces.txt");
+                string[] Labels = labelsInf.Split(',');
+                int.TryParse(Labels[0], out numLablels);
+                string FacesLoad;
+                for (int i = 1; i <= numLablels; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    StaticData.training.Add(new Image<Gray, byte>(Application.StartupPath + $"/faces/{FacesLoad}"));
+                    StaticData.labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nothing in the database");
+            }
+            cam = new Capture();
+            cam.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            frame = cam.QueryFrame().Resize(640, 480, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            GrayFace = frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = GrayFace.DetectHaarCascade(faceDetect, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (StaticData.training.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriteria = new MCvTermCriteria(numLablels, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(StaticData.training.ToArray(), StaticData.labels.ToArray(), 1500, ref termCriteria);
+                    name = recognizer.Recognize(result);
+                    //frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+            }
+            imageBox1.Image = frame;
+        }
+
+        private void RegisterButton_CLicked(object sender, EventArgs e)
+        {
+            GrayFace = cam.QueryGrayFrame().Resize(640, 480, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] DetectedFaces = GrayFace.DetectHaarCascade(faceDetect, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            if (DetectedFaces[0].Length == 0)
+                return;
+            foreach (MCvAvgComp f in DetectedFaces[0])
+            {
+                TrainImg = frame.Copy(f.rect).Convert<Gray, byte>();
+                break;
+            }
+            numLablels++;
+            TrainImg = TrainImg.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            StaticData.training.Add(TrainImg);
+            StaticData.labels.Add(textBox1.Text);
+            //StaticData.passwords.Add(textBox2.Text);
+            File.WriteAllText(Application.StartupPath + "/faces/faces.txt", StaticData.training.ToArray().Length + ",");
+            for (int i = 1; i <= numLablels; i++)
+            {
+                StaticData.training.ToArray()[i - 1].Save(Application.StartupPath + "/faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/faces/faces.txt", StaticData.labels.ToArray()[i - 1] + ",");
+            }
+            MessageBox.Show(textBox1.Text + ", Welcome.");
         }
     }
 }
